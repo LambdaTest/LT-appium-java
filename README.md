3e364a095b3a0970c9f8c2ab03505e3df12597bf
shashankj-lt <94521501+shashankj-lt@users.noreply.github.com>
2022-04-26 21:51:59 +0530

    Added the files

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
 # LT-appium-java
+Lt appium java
+
+## Pre-requisites
+
+---
+
+Before you can start performing App automation testing with Appium, you would need to follow these steps:
+
+- You will need a LambdaTest username and access key. To obtain your access credentials, purchase a plan or access the [automation dashboard](https://appautomation.lambdatest.com/).
+- Ensure you have Appium’s [Java client](https://github.com/appium/java-client) library installed.
+- Ensure you have Maven installed: 
+  - If Maven is not downloaded, you can download it from [here](https://maven.apache.org/download.cgi) 
+  - For installation steps, follow the instructions [here](https://maven.apache.org/install.html)
+  
+### Clone The Sample Project
+
+**Step-1:** Clone the LambdaTest's [LT-appium-java](https://github.com/LambdaTest/LT-appium-java) and navigate to the code directory as shown below:
+
+```bash
+git clone https://github.com/LambdaTest/LT-appium-java
+cd LT-appium-java
+```
+
+
+### Setting Up Your Authentication
+
+Make sure you have your LambdaTest credentials with you to run test automation scripts on LambdaTest. To obtain your access credentials, [purchase a plan](https://billing.lambdatest.com/billing/plans) or access the [Automation Dashboard](https://appautomation.lambdatest.com/).
+
+**Step-2:** Set LambdaTest `Username` and `Access Key` in environment variables.
+
+```bash
+export LT_USERNAME="YOUR_LAMBDATEST_USERNAME"
+export LT_ACCESS_KEY="YOUR_LAMBDATEST_ACCESS_KEY"
+```
+
+### Upload Your Application
+
+**Step-3:** Upload your **_iOS_** application (.ipa file) or **_Android_** application (.apk file) to the LambdaTest servers using our **REST API**. You need to provide your **Username** and **AccessKey** in the format `Username:AccessKey` in the **cURL** command for authentication. Make sure to add the path of the **appFile** in the cURL request. Here is an example cURL request to upload your app using our REST API:
+
+```bash
+curl -u "YOUR_LAMBDATEST_USERNAME":"YOUR_LAMBDATEST_ACCESS_KEY" \
+--location --request POST 'https://manual-api.lambdatest.com/app/upload/realDevice' \
+--form 'name="Android_App"' \
+--form 'appFile=@"/Users/macuser/Downloads/proverbial_android.apk"' 
+```
+
+> **Note:**
+>
+> - If you do not have any **.apk** or **.ipa** file, you can run your sample tests on LambdaTest by using our sample [Android app](https://prod-mobile-artefacts.lambdatest.com/assets/docs/proverbial_android.apk) or sample [iOS app](https://prod-mobile-artefacts.lambdatest.com/assets/docs/proverbial_ios.ipa).
+> - Response of above cURL will be a **JSON** object containing the `App URL` of the format - <lt://APP123456789123456789> and will be used in the next step.
+
+### Executing The Tests
+
+**Step-4:**
+Install the dependencies, by running the following command in the project's base folder
+```bash
+mvn clean install
+```
+**Step-5:**
+Run the following command in the directory where your project has been saved to execute your build.
+- Android
+```bash
+mvn compile exec:java -Dexec.mainClass=vanilla_android -Dexec.classpathScope="test"
+```
+- iOS
+```bash
+mvn compile exec:java -Dexec.mainClass=vanilla_ios -Dexec.classpathScope="test"
+```
+
+Your test results would be displayed on the test console (or command-line interface if you are using terminal/cmd) and on the [LambdaTest App Automation Dashboard](https://appautomation.lambdatest.com/build).
---
+++ src/test/java/vanilla_android.java
@@ -0,0 +1,86 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class vanilla_android {
+    public static String userName = System.getenv("LT_USERNAME") == null ? "shashankj"  //Add username here
+            : System.getenv("LT_USERNAME");
+    public static String accessKey = System.getenv("LT_ACCESS_KEY") == null ? "mPh06ySlwYZZPiVDuAgWBZAXLrq0s5vCLgf8JjaACxHI3bFiUM" //Add accessKey here
+            : System.getenv("LT_ACCESS_KEY");
+
+    private static AppiumDriver driver;
+
+    public static void main(String args[]) throws MalformedURLException, InterruptedException {
+
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("deviceName", "Galaxy S20");
+            capabilities.setCapability("platformVersion", "11");
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("isRealMobile", true);
+            capabilities.setCapability("app", "lt://APP100202491650550026383902"); //Enter your app url
+            capabilities.setCapability("deviceOrientation", "PORTRAIT");
+            capabilities.setCapability("build", "Java Vanilla - iOS");
+            capabilities.setCapability("name", "Sample Test Java");
+            capabilities.setCapability("console", true);
+            capabilities.setCapability("network", false);
+            capabilities.setCapability("visual", true);
+            capabilities.setCapability("devicelog", true);
+
+            driver = new AppiumDriver(new URL("https://" +userName + ":" + accessKey + "@beta-hub.lambdatest.com/wd/hub"), capabilities);
+
+            MobileElement color = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/color"));
+            color.click();
+
+            MobileElement text = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/Text"));
+            //Changes the text to proverbial
+            text.click();
+
+            //toast is visible
+            MobileElement toast = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/toast"));
+            toast.click();
+
+            //notification is visible
+            MobileElement notification = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/notification"));
+            notification.click();
+
+            //Open the geolocation page
+            MobileElement geo = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/geoLocation"));
+            geo.click();
+            Thread.sleep(5000);
+
+            //takes back to home page
+            MobileElement el3 = (MobileElement) driver.findElementByAccessibilityId("Home");
+
+            driver.navigate().back();
+            Thread.sleep(2000);
+
+            //Takes to speed test page
+            MobileElement speedtest = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/speedTest"));
+            speedtest.click();
+            Thread.sleep(5000);
+
+            driver.navigate().back();
+
+            //Opens the browser
+            MobileElement browser = (MobileElement) driver.findElement(MobileBy.AccessibilityId("Browser"));
+            browser.click();
+
+            MobileElement url = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/url"));
+            url.sendKeys("https://www.lambdatest.com");
+            MobileElement find = (MobileElement) driver.findElement(MobileBy.id("com.lambdatest.proverbial:id/find"));
+            find.click();
+
+        } catch (AssertionError a) {
+            ((JavascriptExecutor) driver).executeScript("lambda-status=failed");
+            a.printStackTrace();
+        }
+// The driver.quit statement is required, otherwise the test continues to execute, leading to a timeout.
+        driver.quit();
+    }
+    }
